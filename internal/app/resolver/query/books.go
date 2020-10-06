e9e1d5503fda59b03d48d1a1f072a73953f27db0
Phong Truong <phong.t@kbtg.tech>
2020-10-06 09:57:03 +0700

    Remove unused code

--- internal/app/resolver/query/books.go
+++ internal/app/resolver/query/books.go
@@ -39,19 +39,3 @@ var Books = &graphql.Field{
 		return utils.HandleResp(resp, &books)
 	},
 }
-
-//token := p.Context.Value("token").(string)
-//err := header.ValidateJWT(token)
-//if err != nil {
-//	return nil, err
-//}
-//url := "http://localhost:8080/api/v1/books"
-//resp, err := helper.MakeRequest(http.MethodGet, url, nil)
-//if err != nil {
-//	return []model.Book{}, err
-//}
-//defer resp.Body.Close()
-//
-//var books []model.Book
-//json.NewDecoder(resp.Body).Decode(&books)
-//return books, nil
